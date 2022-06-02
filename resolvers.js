040ff1bad448d268aebe2c8fdf6f9d04d12de435
kiba-dev <kibamaster0301@gmail.com>
2022-06-02 08:08:32 -0700

    add student schema and resolver

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,7 @@
+const db = require("./db")
+
 const Query = {
-    test: () => 'Test Success, GraphQL server is up & running !!'
+    test: () => 'Test Success, GraphQL server is up & running !!',
+    students: () => db.students.list()
 }
 module.exports = { Query }
